73d19e99b11387bc67ea7a199283f8ad092d8396
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-10 15:42:22 +0200

    feat: create array and show the single item in console

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// funzionalità per il carosello
+
+
+// devo ricreare i div con le immagini, perché se il mio DB aumenta o diminuisce questo si modifica in automatico
+
+
+// creao il mio array con le immagini al loro interno (poi andrà a prenderlo da un DB esterno)
+
+const imagineArray = [
+    "img/01.webp", 
+    "img/02.webp", 
+    "img/03.webp", 
+    "img/04.webp", 
+    "img/05.webp"
+];
+
+// prendo i singoli elementi del mio array -> ciclo FOR
+for(i = 0; i< imagineArray.length; i++){
+    console.log(imagineArray[i]);
+    // per ogni i questo ciclo mi deve creare un div con le immagini
+    
+}
